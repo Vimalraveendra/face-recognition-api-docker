f47914cf23329f8397d7201caeb26f25d8e577cb
vimal Raveendran <vimalraveendra@gmail.com>
2020-08-12 23:09:45 +0200

    Added validation to register & sigin endpoint and shifted the apikeys from frontend to backend as well

--- Controllers/signin.js
+++ Controllers/signin.js
@@ -6,17 +6,22 @@ const handleSignIn = (req, res, bcrypt, db) => {
        features when responding with json
       */
   // res.send("signinn");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json("Incorrect form submission");
+  }
 
   db.select("email", "hash")
     .from("login")
-    .where("email", "=", req.body.email)
+    .where("email", "=", email)
     .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash); // true
+      const isValid = bcrypt.compareSync(password, data[0].hash); // true
       if (isValid) {
         return db
           .select("*")
           .from("users")
-          .where("email", "=", req.body.email)
+          .where("email", "=", email)
           .then((user) => res.json(user[0]))
           .catch((err) => res.status(400).json("Unable to get user"));
       } else {
